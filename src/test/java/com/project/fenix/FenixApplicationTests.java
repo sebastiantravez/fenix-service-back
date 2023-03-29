789b40f1fe2786b78683da53315a5ff821050f27
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2023-03-29 14:50:22 -0500

    big bang

---
+++ src/test/java/com/project/fenix/FenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
